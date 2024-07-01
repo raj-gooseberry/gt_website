d33ce9b665fc9d22f9e1cac3cf2b9af8f89af8ab
JYOTY BHUSHAN SINHA <89442990+hunnysinha12@users.noreply.github.com>
2024-07-01 12:26:37 +0530

    updated navbar and sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from 'next';
-
+'use client';
+// import type { Metadata } from 'next';
 import { SITE } from '~/config.js';
-
+import React, { useState, useEffect } from 'react';
 import Hero from '~/components/widgets/Hero2';
 import SocialProof from '../src/components/widgets/SocialProof';
 import Features from '~/components/widgets/Features4';
@@ -13,6 +13,7 @@ import Pricing from '~/components/widgets/Pricing';
 import Team from '~/components/widgets/Team';
 import CallToAction from '~/components/widgets/CallToAction2';
 import Contact from '~/components/widgets/Contact';
+import SectionFirst from '~/components/widgets/SectionFirst';
 import {
   callToAction2Home,
   contactHome,
@@ -29,14 +30,52 @@ import {
   testimonials2About,
 } from '~/shared/data/pages/home.data';
 import Testimonials2 from '~/components/widgets/Testimonials2';
+import SectionSecond from '~/components/widgets/SectionSecond';
+import SectionThird from '~/components/widgets/SectionThird';
+import SectionFourth from '~/components/widgets/SectionFourth';
+import SectionFifth from '~/components/widgets/SectionFifth';
 
-export const metadata: Metadata = {
-  title: SITE.title,
-};
+// export const metadata: Metadata = {
+//   title: SITE.title,
+// };
 
+const sections = [
+  { name: 'First', href: 'first' },
+  { name: 'Second', href: 'second' },
+  { name: 'Third', href: 'third' },
+  { name: 'Fourth', href: 'fourth' },
+  { name: 'Fifth', href: 'fifth' },
+];
+
+function App() {
+  const [activeSection, setActiveSection] = useState('first');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY + window.innerHeight / 2;
+
+      const currentSection = sections.find((section) => {
+        const element = document.getElementById(section.href);
+        return element.offsetTop <= scrollPosition && element.offsetTop + element.offsetHeight > scrollPosition;
+      });
+
+      if (currentSection) {
+        setActiveSection(currentSection.href);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+}
 export default function Page() {
   return (
     <>
+      <SectionFirst />
+      <SectionSecond />
+      <SectionThird />
+      <SectionFourth />
+      <SectionFifth />
       <Hero {...heroHome} />
       <CallToAction {...callToAction2Home} />
       <SocialProof {...socialProofHome} />
